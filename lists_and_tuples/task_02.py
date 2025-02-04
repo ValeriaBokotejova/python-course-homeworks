99d0fd461b65d703fefc469b8e69bbcda8c3b832
Valeria Bokotejova <130553912+ValeriaBokotejova@users.noreply.github.com>
2025-02-04 15:10:05 +0100

    Update lists_and_tuples/task_02.py
    Co-authored-by: Vadym Volkov <72186851+Wadosmp4@users.noreply.github.com>

--- lists_and_tuples/task_02.py
+++ lists_and_tuples/task_02.py
@@ -3,8 +3,7 @@ import string
 
 all_characters = list(string.ascii_lowercase + string.ascii_uppercase + string.digits)
 
-password = ""
-for _ in range(8):
-    password += random.choice(all_characters)
+random_pass = random.choices(all_letters, k=8)
+password = ''.join(random_pass)
 
 print("Generated password:", password)
