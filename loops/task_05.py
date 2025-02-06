608081be2e46cfd40685c94401076a0b515bce66
Valeria Bokotejova <130553912+ValeriaBokotejova@users.noreply.github.com>
2025-02-06 15:14:11 +0100

    Update loops/task_05.py
    Co-authored-by: Vadym Volkov <72186851+Wadosmp4@users.noreply.github.com>

--- loops/task_05.py
+++ loops/task_05.py
@@ -1,9 +1,7 @@
 str_list = ['apple', 'banana', 'cherry', 'date', 'elderberry']
 print(str_list)
 
-reversed_str_list = []
-for i in range(len(str_list) - 1, -1, -1):  
-    reversed_str_list.append(str_list[i])
+reversed_str_list = str_list[::-1] # [begin:end:step]
 
 for index, fruit in enumerate(reversed_str_list):
     print(f"Index {index}: {fruit}")
